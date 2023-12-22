b14ccd52a0dd7c46d1474f53eaf296a185a6379e
adib0303 <71843941+adib0303@users.noreply.github.com>
2023-12-22 12:12:13 +0600

    Add files via upload

---
+++ sources/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+
+#include "../include/global.h"
+#include "../include/sdlshape.h"
+#include "../include/sdlevent.h"
+#include "../include/sdlrender.h"
+#include "../include/starter.h"
+#include "../include/credit.h"
+#include "../include/copyofMain.h"
+
+int main(int argc, char *argv[])
+{
+    // direct calling a function from copyofMain.c
+
+    callFunction();
+    return 0;
+}
